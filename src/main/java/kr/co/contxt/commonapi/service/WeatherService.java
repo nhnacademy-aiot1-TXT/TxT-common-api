a1697a9c52ba138331bb7aa3a4442ed4427034ca
pass0210 <pass0210@gmail.com>
2024-04-19 16:12:20 +0900

    Add: WeatherService 구현
    날씨 정보를 json string 형태로 받아온 후 parsing을 통해 원하는 정보를 dto 형태로 만들어주도록 구현

---
+++ src/main/java/kr/co/contxt/commonapi/service/WeatherService.java
@@ -0,0 +1,7 @@
+package kr.co.contxt.commonapi.service;
+
+import kr.co.contxt.commonapi.dto.WeatherDto;
+
+public interface WeatherService {
+    WeatherDto getWeather();
+}
---
+++ src/main/java/kr/co/contxt/commonapi/service/impl/WeatherServiceImpl.java
@@ -0,0 +1,46 @@
+package kr.co.contxt.commonapi.service.impl;
+
+import kr.co.contxt.commonapi.adaptor.WeatherAdaptor;
+import kr.co.contxt.commonapi.dto.WeatherDto;
+import kr.co.contxt.commonapi.properties.WeatherApiProperties;
+import kr.co.contxt.commonapi.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class WeatherServiceImpl implements WeatherService {
+    private final Map<String, String> skyMap;
+    private final WeatherAdaptor weatherAdaptor;
+    private final WeatherApiProperties weatherApiProperties;
+
+    @Override
+    public WeatherDto getWeather() {
+        LocalDateTime dateTime = LocalDateTime.now().minusHours(1);
+        String weatherInfo = weatherAdaptor.getWeatherInfo(
+                weatherApiProperties.getServiceKey(),
+                weatherApiProperties.getPageNo(),
+                weatherApiProperties.getNumOfRows(),
+                weatherApiProperties.getDataType(),
+                dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd")),
+                dateTime.format(DateTimeFormatter.ofPattern("HHmm")),
+                weatherApiProperties.getNx(),
+                weatherApiProperties.getNy()
+        );
+
+        JSONArray jsonArray = new JSONObject(weatherInfo).getJSONObject("response")
+                .getJSONObject("body")
+                .getJSONObject("items")
+                .getJSONArray("item");
+        JSONObject temperatureInfo = jsonArray.getJSONObject(0);
+        JSONObject skyInfo = jsonArray.getJSONObject(5);
+
+        return new WeatherDto(temperatureInfo.getFloat("fcstValue"), skyMap.get(skyInfo.getString("fcstValue")));
+    }
+}
