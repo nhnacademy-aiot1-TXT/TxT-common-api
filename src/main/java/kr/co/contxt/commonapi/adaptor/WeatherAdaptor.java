a9638a267f82b41508b99a3a66b57715b1db46d2
pass0210 <pass0210@gmail.com>
2024-04-19 18:19:56 +0900

    Chore: 단기 예보에서 초단기 예보로 변경

--- src/main/java/kr/co/contxt/commonapi/adaptor/WeatherAdaptor.java
+++ src/main/java/kr/co/contxt/commonapi/adaptor/WeatherAdaptor.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "weather-api", url = "http://apis.data.go.kr", path = "/1360000/VilageFcstInfoService_2.0/getVilageFcst")
+@FeignClient(name = "weather-api", url = "http://apis.data.go.kr", path = "/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst")
 public interface WeatherAdaptor {
     @GetMapping
     String getWeatherInfo(
