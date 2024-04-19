7caf36fcc4a1fb3e5ead10f0e3ec562890fe0160
jongsikk <xxx_@kakao.com>
2024-04-19 16:56:45 +0900

    Feat: FeignClient dependency 추가 및 설정

--- src/main/java/kr/co/contxt/commonapi/CommonApiApplication.java
+++ src/main/java/kr/co/contxt/commonapi/CommonApiApplication.java
@@ -2,12 +2,14 @@ package kr.co.contxt.commonapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CommonApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApiApplication.class, args);
+    }
 
 }
