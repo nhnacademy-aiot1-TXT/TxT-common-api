68ffb6aecbefe08d21e61297d19e7a48c9605c65
pass0210 <pass0210@gmail.com>
2024-04-19 16:06:36 +0900

    Refactor: CommonApiApplication 수정
    - Eureka client 등록을 위한 annotation 추가
    - OpenFeign을 사용하기 위한 annotation 추가
    - 설정파일을 읽기 위한 annotation 추가

--- src/main/java/kr/co/contxt/commonapi/CommonApiApplication.java
+++ src/main/java/kr/co/contxt/commonapi/CommonApiApplication.java
@@ -1,13 +1,20 @@
 package kr.co.contxt.commonapi;
 
+import kr.co.contxt.commonapi.properties.PropertiesBase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackageClasses = PropertiesBase.class)
 public class CommonApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApiApplication.class, args);
+    }
 
 }
