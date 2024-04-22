e0e98603b4cca55ffdcea56cfb4410aecb4d4dc1
jongsikk <xxx_@kakao.com>
2024-04-22 13:26:37 +0900

    Add: Device Entity 추가

--- src/main/java/kr/co/contxt/commonapi/entity/Device.java
+++ src/main/java/kr/co/contxt/commonapi/entity/Device.java
@@ -1,18 +1,14 @@
 package kr.co.contxt.commonapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import kr.co.contxt.commonapi.dto.DeviceResponse;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalTime;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,9 +16,18 @@ import java.time.LocalTime;
 public class Device {
     @Id
     @Column(name = "device_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "device_name")
     private String name;
     @Column
     LocalTime cycle;
+
+    public DeviceResponse toDto() {
+        return DeviceResponse.builder()
+                .deviceId(id)
+                .deviceName(name)
+                .cycle(cycle)
+                .build();
+    }
 }
