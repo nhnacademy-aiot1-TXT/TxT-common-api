56f1f945a5b361efa225aa0c054e84c9a3f2e17d
pass0210 <pass0210@gmail.com>
2024-04-23 15:05:56 +0900

    Docs: SensorRepository JavaDoc 추가

--- src/main/java/kr/co/contxt/commonapi/repository/SensorRepository.java
+++ src/main/java/kr/co/contxt/commonapi/repository/SensorRepository.java
@@ -3,6 +3,18 @@ package kr.co.contxt.commonapi.repository;
 import kr.co.contxt.commonapi.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * 센서 테이블 접근을 위한 JpaRepository interface
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
+    /**
+     * 센서 이름이 존재하는지 확인하는 메서드
+     *
+     * @param sensorName 센서 이름
+     * @return 존재 여부
+     */
     boolean existsBySensorName(String sensorName);
 }
