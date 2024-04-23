98545a90c181c980808da269786dbe716747a5a9
pass0210 <pass0210@gmail.com>
2024-04-23 22:50:35 +0900

    Refactor: DeviceSensorService 수정
    장비 이름별 센서 리스트 조회 메서드 추가
    장비 이름, 센서 이름별 센서 단일 조회 메서드 추가

--- src/main/java/kr/co/contxt/commonapi/service/DeviceSensorService.java
+++ src/main/java/kr/co/contxt/commonapi/service/DeviceSensorService.java
@@ -1,5 +1,7 @@
 package kr.co.contxt.commonapi.service;
 
+import kr.co.contxt.commonapi.dto.DeviceAndSensorNameDto;
+import kr.co.contxt.commonapi.dto.DeviceNameDto;
 import kr.co.contxt.commonapi.dto.DeviceSensorResponse;
 
 import java.util.List;
@@ -7,5 +9,9 @@ import java.util.List;
 public interface DeviceSensorService {
     List<DeviceSensorResponse> getSensorListByDevice(Long deviceId);
 
+    List<DeviceSensorResponse> getSensorListByDevice(DeviceNameDto deviceNameDto);
+
     DeviceSensorResponse getSensorByDeviceAndSensor(Long deviceId, Long sensorId);
+
+    DeviceSensorResponse getSensorByDeviceAndSensor(DeviceAndSensorNameDto deviceAndSensorNameDto);
 }
