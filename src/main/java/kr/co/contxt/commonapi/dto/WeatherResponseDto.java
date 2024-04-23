cbf0a04502cc6a647c216d0cc400294aa9bf6970
pass0210 <pass0210@gmail.com>
2024-04-23 14:20:34 +0900

    Docs: WeatherResponseDto JavaDoc 추가

--- src/main/java/kr/co/contxt/commonapi/dto/WeatherResponseDto.java
+++ src/main/java/kr/co/contxt/commonapi/dto/WeatherResponseDto.java
@@ -5,6 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * 날씨 온도 응답 Dto
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 @Getter
 @Setter
 @NoArgsConstructor
