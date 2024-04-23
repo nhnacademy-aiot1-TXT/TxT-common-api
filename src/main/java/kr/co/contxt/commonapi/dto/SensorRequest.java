48655c7cf1d04324d7843fb34fab3be55a83edc9
pass0210 <pass0210@gmail.com>
2024-04-23 15:06:03 +0900

    Docs: SensorRequest JavaDoc 추가

--- src/main/java/kr/co/contxt/commonapi/dto/SensorRequest.java
+++ src/main/java/kr/co/contxt/commonapi/dto/SensorRequest.java
@@ -5,12 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 센서 정보 요청 dto class
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SensorRequest {
     private String sensorName;
 
+    /**
+     * dto를 entity로 변환하는 메서드
+     *
+     * @return 센서 entity
+     */
     public Sensor toEntity() {
         return Sensor.builder()
                 .sensorName(sensorName)
