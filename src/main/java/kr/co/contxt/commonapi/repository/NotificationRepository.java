7f39fc2405ff9c048529af399fda48d048674b89
jongsikk <xxx_@kakao.com>
2024-04-23 14:22:52 +0900

    Docs: NotifictionRepository JavaDoc 작성

--- src/main/java/kr/co/contxt/commonapi/repository/NotificationRepository.java
+++ src/main/java/kr/co/contxt/commonapi/repository/NotificationRepository.java
@@ -5,8 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Notification Table 접근을 위한 JpaRepository interface
+ *
+ * @author jongsikk
+ * @version 1.0.0
+ */
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findAll();
 
+    /**
+     * Find by role id list.
+     *
+     * @param roleId the role id
+     * @return notification list
+     */
     List<Notification> findByRole_Id(Long roleId);
 }
