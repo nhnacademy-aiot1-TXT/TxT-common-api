eec2bcd2d4016b8e92fbfac8f539e943096c9705
jongsikk <xxx_@kakao.com>
2024-04-25 21:21:27 +0900

    Refactor: exception package 리팩토링

--- src/main/java/kr/co/contxt/commonapi/exception/TimeIntervalNotFoundException.java
+++ src/main/java/kr/co/contxt/commonapi/exception/TimeIntervalNotFoundException.java
@@ -1,9 +1,21 @@
 package kr.co.contxt.commonapi.exception;
 
+/**
+ * TimeInterval 정보가 없을 때 발생하는 Exception 클래스
+ */
 public class TimeIntervalNotFoundException extends RuntimeException {
+    /**
+     * 기본생성자
+     */
     public TimeIntervalNotFoundException() {
     }
 
-    public TimeIntervalNotFoundException(String s) {
+    /**
+     * message를 가지는 생성자
+     *
+     * @param message the message
+     */
+    public TimeIntervalNotFoundException(String message) {
+        super(message);
     }
 }
