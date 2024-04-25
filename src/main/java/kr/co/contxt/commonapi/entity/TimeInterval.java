3128f553ece2886c48ac3da8d66490b3ee97e024
pass0210 <pass0210@gmail.com>
2024-04-25 17:40:57 +0900

    Add: TimeInterval entity 추가

---
+++ src/main/java/kr/co/contxt/commonapi/entity/TimeInterval.java
@@ -0,0 +1,45 @@
+package kr.co.contxt.commonapi.entity;
+
+import kr.co.contxt.commonapi.dto.TimeIntervalResponse;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+
+/**
+ * TimeInterval Entity
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "time_interval")
+public class TimeInterval {
+    @Id
+    @Column(name = "time_interval_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long timeIntervalId;
+    @ManyToOne
+    @JoinColumn(name = "sensor_id")
+    private Sensor sensor;
+    private LocalTime begin;
+    private LocalTime end;
+
+    /**
+     * To dto time interval response.
+     *
+     * @return the time interval response
+     */
+    public TimeIntervalResponse toDto() {
+        return TimeIntervalResponse.builder()
+                .sensorName(sensor.getSensorName())
+                .begin(begin)
+                .end(end)
+                .build();
+    }
+}
