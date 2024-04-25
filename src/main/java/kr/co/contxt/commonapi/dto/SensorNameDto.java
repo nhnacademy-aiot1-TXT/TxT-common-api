c4f38a9662a83ac1faee323f45ece7eb5b80801e
pass0210 <pass0210@gmail.com>
2024-04-25 17:40:27 +0900

    Add: SensorNameDto 추가

---
+++ src/main/java/kr/co/contxt/commonapi/dto/SensorNameDto.java
@@ -0,0 +1,18 @@
+package kr.co.contxt.commonapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * The type Sensor name dto.
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SensorNameDto {
+    private String sensorName;
+}
